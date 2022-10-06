b8201e66f53c731b29e3e525f4fa15cf8e63a867
vishal pal <vishalpal6486@gmail.com>
2022-10-06 14:20:34 +0530

    Project Completed

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  selectedIndex: number = -1;
+
+  onVoteClick(index: number){
+    if (this.isSelected(index)){
+      this.selectedIndex = -1;
+    }else{
+      this.selectedIndex = index;
+    }
+  }
+
+  isSelected(index: number){
+    return this.selectedIndex === index;
+  }
+
+  anySelected(){
+    return this.selectedIndex !== -1;
+  } 
+  
+
+  getButtonText(index: number){
+    if (this.isSelected(index)){
+      return "Unvote";
+    } else{
+      return "Vote";
+    }
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MovieDisplayComponent } from './movie-display/movie-display.component';
+import { FlopVotesComponent } from './flop-votes/flop-votes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MovieDisplayComponent,
+    FlopVotesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
